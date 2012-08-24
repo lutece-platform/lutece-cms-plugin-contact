f9a3c986e95be17077b9900ad50413bc734c7590
pierrelevy <pierrelevy@users.noreply.github.com>
2012-08-24 00:59:13 +0000

    CONTACT-36 : Fix SQL init script to provide the feature automaticaly to Admin and Webmaster users
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-contact@43374 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/contact/core/init_core_contact.sql
+++ src/sql/plugins/contact/core/init_core_contact.sql
@@ -4,3 +4,11 @@
 
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url) VALUES 
 ('CONTACT_MANAGEMENT','contact.adminFeature.contact_management.name',3,'jsp/admin/plugins/contact/ManageContactsHome.jsp','contact.adminFeature.contact_management.description',0,'contact','APPLICATIONS','images/admin/skin/plugins/contact/contact.png', NULL);
+
+--
+-- Dumping data for table core_user_right
+--
+INSERT INTO core_user_right (id_right,id_user) VALUES ('CONTACT_MANAGEMENT',1);
+INSERT INTO core_user_right (id_right,id_user) VALUES ('CONTACT_MANAGEMENT',2);
+
+
