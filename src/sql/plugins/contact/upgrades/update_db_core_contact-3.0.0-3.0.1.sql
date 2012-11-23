549716c80effe70f7ba1009cb19ccf2771f80bb5
hohll <hohll@users.noreply.github.com>
2012-11-23 14:40:15 +0000

    LUTECE-1493 : Bootstrap CSS Framework integration plugin contact update sql init file to access to manage contact feature instead of special home page "manage contact + manage contact list"
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-contact@45723 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/contact/upgrades/update_db_core_contact-3.0.0-3.0.1.sql
+++ src/sql/plugins/contact/upgrades/update_db_core_contact-3.0.0-3.0.1.sql
@@ -1,14 +1,4 @@
---
--- Dumping data for table core_admin_right
---
-
+DELETE FROM core_admin_right WHERE id_right='CONTACT_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url) VALUES 
 ('CONTACT_MANAGEMENT','contact.adminFeature.contact_management.name',3,'jsp/admin/plugins/contact/ManageContacts.jsp','contact.adminFeature.contact_management.description',0,'contact','APPLICATIONS','images/admin/skin/plugins/contact/contact.png', NULL);
-
---
--- Dumping data for table core_user_right
---
-INSERT INTO core_user_right (id_right,id_user) VALUES ('CONTACT_MANAGEMENT',1);
-INSERT INTO core_user_right (id_right,id_user) VALUES ('CONTACT_MANAGEMENT',2);
-
 
