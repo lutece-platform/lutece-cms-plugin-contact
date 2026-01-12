066eefd727e1f73627b3fdd98527d464a560694f
hohll <laurent.hohl@gmail.com>
2026-01-12 16:56:18 +0100

    LUT-31104 Migration lutece 8

--- src/sql/plugins/contact/core/init_core_contact.sql
+++ src/sql/plugins/contact/core/init_core_contact.sql
@@ -6,7 +6,7 @@
 --
 
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url) VALUES 
-('CONTACT_MANAGEMENT','contact.adminFeature.contact_management.name',3,'jsp/admin/plugins/contact/ManageContacts.jsp','contact.adminFeature.contact_management.description',0,'contact','APPLICATIONS','images/admin/skin/plugins/contact/contact.png', NULL);
+('CONTACT_MANAGEMENT','contact.adminFeature.contact_management.name',3,'jsp/admin/plugins/contact/ManageContacts.jsp','contact.adminFeature.contact_management.description',0,'contact','APPLICATIONS','ti ti-address-book', NULL);
 
 --
 -- Dumping data for table core_user_right
