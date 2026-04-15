ec04e98a099e6da27ce2c8afe4b0c4dd99aff68e
NicolasJnn <nicolas.jouanne@paris.fr>
2026-04-15 16:26:04 +0200

    LUT-32363: Use Reply-To header for visitor email instead of SMTP From

--- src/java/fr/paris/lutece/plugins/contact/web/ContactApp.java
+++ src/java/fr/paris/lutece/plugins/contact/web/ContactApp.java
@@ -501,7 +501,7 @@ public class ContactApp extends MVCApplication
 
         String strMessageText = template.getHtml( );
 
-        MailService.sendMailHtml( strEmailContact, strVisitorLastName, strVisitorEmail, strObject, strMessageText );
+        MailService.sendMailHtml( strEmailContact, strVisitorLastName, MailService.getNoReplyEmail( ), strObject, strMessageText, strVisitorEmail );
         ContactHome.updateHits( nContact, nIdContactList, _plugin );
 
         Map<String, String> mapParamSuccess = new HashMap<String, String>( );
